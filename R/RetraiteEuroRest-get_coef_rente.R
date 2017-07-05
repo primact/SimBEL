f4161e58a06903955cfdb360adb9c00fa83aeb05
qguibert <q.guibert@hotmail.fr>
2017-07-05 17:00:07 +0200

    Version 2.0.0 : Correction documentation.

--- R/RetraiteEuroRest-get_coef_rente.R
+++ R/RetraiteEuroRest-get_coef_rente.R
@@ -6,24 +6,14 @@
 ##' \code{get_coef_rente} est une methode permettant d'obtenir le coefficient de survie esperee actualise au taux technique de rente.
 ##' @name get_coef_rente
 ##' @docType methods
-##' @param x un objet de la classe \code{\link{HypTech}}.
-##' @param nom_table_1 un \code{character} designant le nom de la table de mortalite de la tete principale.
-##' @param age_1 la valeur \code{integer} de l'age de la tete principale.
-##' @param gen_1 la valeur \code{integer} de la generation de la tete principale.
-##' @param statut l'indicatrice \code{integer} permettant de determiner le statut de la rente
-##'  (1 : une tete, 2 : une tete principale et un reversataire, 3 : seul le reversataire est en vie).
-##' @param tx_rvs un \code{numeric} correspondant au taux de reversion de la rente. 
-##' @param tx_tech un \code{numeric} correspondant au taux technique d'actualisation du coefficient actuariel.
-##' @param nom_table_2 un \code{character} designant le nom de la table de mortalite de la tete secondaire.
-##' @param age_2 la valeur \code{integer} de l'age de la tete secondaire.
-##' @param gen_2 la valeur \code{integer} de la generation de la tete secondaire.
-##' @param freq_rente un \code{integer} correspondant au nombre de versements dans l'annee.
-##' @param echu un \code{logical} correspond a la methode de calcul du coefficient actuariel.
-##' @return Le coefficient \code{numeric} d'ax ou axy selon le statut du model point dont les caractéristiques sont precisees en input.
+##' @param x un objet de la classe \code{\link{RetraiteEuroRest}}.
+##' @param ht un objet de la classe \code{\link{HypTech}}.
+##' @return Le coefficient \code{numeric} d'ax ou axy selon le statut du model point dont les caracteristiques sont precisees en input.
 ##' @author Prim'Act
 ##' @export
 ##' @include RetraiteEuroRest_class.R
-##' 
+##'
+
 setGeneric("get_coef_rente", function(x, ht) {standardGeneric("get_coef_rente")})
 setMethod(
     f = "get_coef_rente",
@@ -31,13 +21,13 @@ setMethod(
     def = function(x, ht){
 
         epi_mp <- x@mp
-        
+
         # Nombre de lignes
         nb_mp <- nrow(x@mp)
-        
+
         # Gestion des noms de colonnes du data.frame de donnnees
         nom_epi <- names(epi_mp)
-        
+
         # Recuperer numeros colonnes
         num_tab_mort     <- which(nom_epi == "num_tab_mort")
         num_tab_mort_rvs <- which(nom_epi == "num_tab_mort_rvs")
@@ -50,11 +40,11 @@ setMethod(
         num_tx_tech      <- which(nom_epi == "tx_tech")
         num_freq         <- which(nom_epi == "freq_rente")
         num_echu         <- which(nom_epi == "echu")
-        
-        
+
+
         # Fonction d'extraction des taux de sortie
         calc_coef_rente_mp <- function(i) {
-            
+
             #Age du model point i
             tab_mort_i     <- as.character(.subset2(epi_mp, num_tab_mort)[i])
             tab_mort_rvs_i <- as.character(.subset2(epi_mp, num_tab_mort_rvs)[i])
@@ -67,7 +57,7 @@ setMethod(
             tx_tech_i      <- .subset2(epi_mp, num_tx_tech)[i]
             freq_i         <- .subset2(epi_mp, num_freq)[i]
             echu_i         <- .subset2(epi_mp, num_echu)[i]
-            
+
             # Ajout d un test de presence du nom
             if (! (tab_mort_i %in% names(ht@tables_mort)) | ! (tab_mort_rvs_i %in% names(ht@tables_mort))) {
                 stop("[RetraiteEuroRest : get_coef_rente] Nom de table de mortalite non trouve")
@@ -83,8 +73,8 @@ setMethod(
                 }
             }
         }
-        
-        
+
+
         return(t(sapply(1:nb_mp, calc_coef_rente_mp)))
     }
 )
