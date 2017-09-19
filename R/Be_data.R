a4499c9e9d4e69b8b1ea456c6f5c5e867e95dd05
qguibert <q.guibert@hotmail.fr>
2017-09-19 16:41:22 +0200

    Version 3.0.0 : Ajout de donnees exemples.

---
+++ R/Be_data.R
@@ -0,0 +1,18 @@
+# Dataset pour le package SimBEL
+#
+##' Dataset Be.
+##'
+##' Dataset de la classe \code{\link{Be}} contenant des donnees correspondant aux classes : \code{\link{Canton}}, \code{\link{ParamBe}}, \code{\link{ESG}}.
+##'
+##' @name Be_DataSet
+##' @docType data
+##' @usage data(be)
+##' @format Objet de la classe \code{\link{Be}}.
+##' @keywords datasets
+##' @author Prim'Act
+##' @note Il s'agit de donnees simulees, elles ont ete creees afin de tester les differents fonctions du package.
+##' @examples
+##' data(be)
+##' param_be <- be@param_be
+##' portefeuille_passif <- be@canton@ptf_passif
+"be"
---
+++ R/Canton_data.R
@@ -0,0 +1,17 @@
+# Dataset pour le package SimBEL
+#
+##' Dataset Canton.
+##'
+##' Dataset de la classe \code{\link{Canton}} contenant un portefeuille de passif, d'actif ainsi que plusieurs hypothhses.
+##'
+##' @name Canton_DataSet
+##' @docType data
+##' @usage data(canton)
+##' @format Objet de la classe \code{\link{Canton}}.
+##' @keywords datasets
+##' @author Prim'Act
+##' @note Il s'agit de donnees simulees, elles ont ete creees afin de tester les differents fonctions du package.
+##' @examples
+##' data(canton)
+##' portefeuille_passif <- canton@ptf_passif
+"canton"
